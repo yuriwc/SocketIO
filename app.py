9e9685ee946ea16a7773e42b036195e1efbf3e4c
Yuri Cavalcante <matosf.yuri@gmail.com>
2021-10-31 19:20:15 -0300

    some changes

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 io = SocketIO(app, cors_allowed_origins="*", async_handlers=True, pingTimeout=900)
 
+@app.after_request
+def after_request(resp):
+    resp.headers.add('Access-Control-Allow-Origin', '*')
+    resp.headers.add('Access-Control-Allow-Headers', 'Content-Type, X-Token')
+    resp.headers.add('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    return resp
+
+
 @app.route("/")
 def home():
     return render_template("chat.html")
